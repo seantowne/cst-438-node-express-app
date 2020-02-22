352ec4d7220308bf43b99efaf738bf82e0c7def5
EC2 Default User <ec2-user@ip-172-31-27-92.us-east-2.compute.internal>
2020-02-22 05:13:06 +0000

    AutomatedCommitAndPush

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 var port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('index.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
